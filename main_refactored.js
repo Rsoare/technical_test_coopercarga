4ca1a3d039dbaf7f66a28200f2f1da421863ff6d
Rihan Soares <109166924+Rsoare@users.noreply.github.com>
2023-09-02 19:26:52 -0300

    Update main_refactored.js

--- main_refactored.js
+++ main_refactored.js
@@ -1,8 +1,8 @@
 class DoSomething {
 
-   constructor(numberMax, maxOrder) {
+   constructor(numberMax) {
       this.numberMax = numberMax;
-      this.maxOrder = maxOrder;
+      this.maxOrder = 30;
       this.arryPrimeNumber = Array(this.numberMax + 1).fill(0);
       this.arryPrimeNumber[1] = 2;
       this.isPrime = false
@@ -12,17 +12,18 @@ class DoSomething {
       this.rowPerPage = 50;
       this.quantityColumns = 4;
    }
-
+   
+   // Método para encontrar números primos
    searchPrimeNumbers() {
-
+   
       let countLoop = 1;
       let primeSquare = 9;
-
+   
       while (countLoop < this.numberMax) {
          do {
             this.testPrimeNumber += 2;
             if (this.testPrimeNumber == primeSquare) {
-
+   
                this.indexPrime++;
                primeSquare = this.arryPrimeNumber[this.indexPrime] * this.arryPrimeNumber[this.indexPrime];
                this.multiplePrime[this.indexPrime - 1] = this.testPrimeNumber;
@@ -33,11 +34,12 @@ class DoSomething {
          this.arryPrimeNumber[countLoop] = this.testPrimeNumber;
       }
    }
-
+   
+   // Método para encontrar múltiplos de números primos
    findMultiples() {
       let countLoop = 0;
       this.isPrime = true;
-
+   
       while (countLoop < this.indexPrime && this.isPrime) {
          if (this.multiplePrime[countLoop] < this.testPrimeNumber) {
             this.multiplePrime[countLoop] += this.arryPrimeNumber[countLoop] + this.arryPrimeNumber[countLoop];
@@ -48,41 +50,43 @@ class DoSomething {
          countLoop++;
       }
    }
-
+   
+   // Método responsável por imprimir números primos em páginas
    printNumbersPrime() {
       let pageOfSet = 1;
       let currentPage = 0;
-
+   
       while (pageOfSet <= this.numberMax) {
          console.log("Page ", currentPage);
          for (let rowOfSet = pageOfSet; rowOfSet <= pageOfSet + this.rowPerPage - 1; rowOfSet++) {
-
+   
             this.calcIndex(rowOfSet)
          }
          currentPage++;
          pageOfSet += this.rowPerPage * this.quantityColumns;
       }
    }
-
+   
+   // Metodo responsável por calcular o index  e imprimir números primos em uma linha da grade
    calcIndex(rowOfSet) {
       let aux = [];
-
+   
       for (let i = 0; i < this.quantityColumns; i++) {
          const index = rowOfSet + i * this.rowPerPage;
          if (index <= this.numberMax) {
             aux.push(this.arryPrimeNumber[index]);
          }
       }
-
+   
       console.log(aux.join('|'));
    }
-
-
+   
+   
    generatePrimeNumbers() {
       this.searchPrimeNumbers();
       this.printNumbersPrime();
    }
 }
 
-const myApp = new DoSomething(1000, 30);
+const myApp = new DoSomething(1000);
 myApp.generatePrimeNumbers();
